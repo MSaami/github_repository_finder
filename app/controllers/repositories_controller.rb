0f787bb57c2a0a6d5e3693c70eb2ad23652c9597
Mehrdad Saami <MehrdadSaami@gmail.com>
2021-11-02 01:03:59 +0330

    Make new repository model to map items to model

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,13 @@
 class RepositoriesController < ApplicationController
   def index
     @repositories = if params[:q].present?
-      Octokit::Client.new.search_repositories(params[:q], per_page: 100).items
+      items = Octokit::Client.new.search_repositories(params[:q], per_page: 100).items
+      items.map { |item| Repository.new(
+        name: item.full_name,
+        description: item.description,
+        star: item.stargazers_count,
+        url: item.html_url
+      )}
     else
       []
     end
---
+++ app/models/repository.rb
@@ -0,0 +1,10 @@
+class Repository
+  attr_reader :name, :star, :description, :url
+
+  def initialize(name:, star:, description:, url:)
+    @name = name
+    @star = star
+    @description = description
+    @url = url
+  end
+end
